df628ed90175c3ccb3c27bfc50db5fc61fd944d6
Luka Abutidze <lukaabutidze99@gmail.com>
2024-05-02 22:03:07 +0400

    another commit

--- src/app/@quiz/page.tsx
+++ src/app/@quiz/page.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Quiz = () => {
   return (
-    <section className="flex flex-col justify-center items-center p-4">
-      <h4 className="mb-4 text-center  text-xl font-extrabold leading-none tracking-tight md:text-2xl lg:text-4xl  text-blue-600 dark:text-blue-500">
-        HEEEY
-      </h4>
+    <section className="flex flex-col justify-center items-center mt-10">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        HEHEHEHE
+        <span className="text-blue-600 dark:text-blue-500">HHHEEE</span>.
+      </h1>
     </section>
   );
 };
